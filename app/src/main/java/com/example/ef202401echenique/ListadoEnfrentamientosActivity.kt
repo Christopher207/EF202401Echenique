3f6ce5dd1c240b7050783f8008a08044a02c0462
christopher207 <20100172@ue.edu.pe>
2024-07-13 10:10:51 -0500

    [ADD] Full Layouts

---
+++ app/src/main/java/com/example/ef202401echenique/ListadoEnfrentamientosActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ef202401echenique
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListadoEnfrentamientosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listado_enfrentamientos)
+    }
+}
